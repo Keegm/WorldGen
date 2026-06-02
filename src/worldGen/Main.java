c861de9ae75e5b95e27fd7163c0c120ddc28682b
BuildTools <unconfigured@null.spigotmc.org>
2026-06-01 23:16:04 -0400

    Added rock collision and added sprint control

--- src/worldGen/Main.java
+++ src/worldGen/Main.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 
 public class Main {
 	
-	private static boolean up, down, left, right;
+	private static boolean up, down, left, right, sprint;
 	private static float speed = 1;
 
     public static void main(String[] args) {
@@ -32,6 +32,7 @@ public class Main {
         			case KeyEvent.VK_A -> left = true;
         			case KeyEvent.VK_S -> down = true;
         			case KeyEvent.VK_D -> right = true;
+        			case KeyEvent.VK_SHIFT -> sprint = true;
         		
         		
         		}
@@ -43,6 +44,7 @@ public class Main {
         			case KeyEvent.VK_A -> left = false;
         			case KeyEvent.VK_S -> down = false;
         			case KeyEvent.VK_D -> right = false;
+        			case KeyEvent.VK_SHIFT -> sprint = false;
         		
         		
         		}
@@ -52,22 +54,43 @@ public class Main {
         new Timer(16, e -> {
         	float dx = 0, dy = 0;
         	
-        	if (up) dy = -speed;
-        	if (down) dy = speed;
-        	if (left) dx = -speed;
-        	if (right) dx = speed;
-        	
-        	int tilesX = panel.getWidth() / panel.TILE_SIZE;
-        	int tilesY = panel.getHeight() / panel.TILE_SIZE();
         	
         	int pX = (int)Math.floor(panel.camX() + (panel.getWidth() / 2f) / panel.TILE_SIZE());
         	int pY = (int)Math.floor(panel.camY() + (panel.getHeight() / 2f) / panel.TILE_SIZE());
+        	
 
+
+        	
         	Tile tile = world.tileAt(pX, pY);
         	if (tile == Tile.WATER) speed = 1;
         	else speed = 2;
         	
+        	if (sprint) speed += 1;
+        	else speed = 1;
+        	
+        	//Check Top Left and Right
+        	if (up && world.tileAt(pX, pY - 1) != Tile.ROCK 
+        			&& world.tileAt(pX + WorldPanel.TILE_SIZE * 5, pY - 1) != Tile.ROCK) dy = -speed;
+        	//Check Bottom Left and Right
+        	if (down && world.tileAt(pX, pY + WorldPanel.TILE_SIZE * 10 + 1) != Tile.ROCK 
+        			&& world.tileAt(pX + WorldPanel.TILE_SIZE * 5, pY + WorldPanel.TILE_SIZE * 10 + 1) != Tile.ROCK) dy = speed;
+        	//Check Top Left and Bottom Left
+        	if (left && world.tileAt(pX - 1, pY) != Tile.ROCK 
+        		&& world.tileAt(pX - 1, pY + WorldPanel.TILE_SIZE * 10) != Tile.ROCK) dx = -speed;
+        	//Check Top Right and Bottom Right
+        	if (right && world.tileAt(pX + WorldPanel.TILE_SIZE * 5 + 1, pY) != Tile.ROCK
+        			&& world.tileAt(pX + WorldPanel.TILE_SIZE * 5 + 1, pY + WorldPanel.TILE_SIZE * 10) != Tile.ROCK) dx = speed;
+        	
+
+        	
+        	int tilesX = panel.getWidth() / panel.TILE_SIZE;
+        	int tilesY = panel.getHeight() / panel.TILE_SIZE();
+        	
+
+
+        	
         	if (dx != 0 || dy != 0) {
+        		System.out.println(dx);
         		panel.moveCamera(dx, dy);
         	}
         }).start();
